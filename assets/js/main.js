977992ec538af2ca92edc6dadbd86e42b0da6f9c
yasminndhsr <yasminindah462@gmail.com>
2024-07-02 15:55:20 +0700

    feat: perbaikan

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ var data = [];
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-2">
-                                    <img src="${data[i].foto}" class="img-fluid">
+                                    <img src="${data[i].foto}" class="img-fluid mt-1 ms-2">
                                 </div>
                                 <div class="col-10">
                                     <p class="m-0">${data[i].nama}</p>
@@ -36,36 +36,35 @@ const productArea = document.getElementById("product-area");
 var data_product = [];
 
 (() => {
-    fetch("http://192.168.18.12:3000/product", { 
+    fetch("http://192.168.18.12:3000/product",{
         method: "GET",
     }).then(res => res.json()).then(hasil => {
-        data_product = hasil;
-
-        for (let i = 0; i < data_product.length; i++) {
-            if (data_product[i].status == "active") {
-                productArea.innerHTML += `
-                    <div class="col-12 col-xl-6 mb-3">
-                        <div class="card">
-                            <div class="card-product bg-secondary text-light">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <img src="${data_product[i].foto}" alt="Product Image" class="img-fluid img-section">
-                                        </div>
-                                        <div class="col-8">
-                                            <h5>${data_product[i].judul}</h5>
-                                            <p>${data_product[i].isi}</p>
-                                            <a href="${data_product[i].link}" class="btn btn-primary">Klik Disini</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+        console.log(hasil)
+        data_product = hasil
+        for(let i = 0; i < data_product.length; i++){
+            if(data_product[i].status == "active"){
+            productArea.innerHTML +=`
+              <div class="col-12 col-xl-6 mb-5">
+                  <div class="card card-product bg-secondary text-light">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-4">
+                          <img src="${data_product[i].foto}" alt="" 
+                          class="img-fluid"/>
                         </div>
-                    </div>`
-            }
+                        <div class="col-8 mt-2">
+                            <h5 class="fw-bold">${data_product[i].judul}</h5>
+                           ${data_product[i].isi} <br>
+                            <a href="${data_product[i].link}" class="btn btn-primary mt-4">Klik Disini</a>
+                            </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            `
         }
-    })
-})()
+        }})})()
+
 
 
 const dropArea = document.getElementById("dropArea");
